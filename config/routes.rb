1dacf5cf912a9079553065d1b0e45a7f049ea29c
Armando Mendoza <fcoarmandomendoza@gmail.com>
2012-07-18 15:27:50 -0430

    Added ExpensesControllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 MyExpenses::Application.routes.draw do
+
   devise_for :users
+
+  resources :expenses, except: :show
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +53,7 @@ MyExpenses::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'expenses#index'
 
   # See how all your routes lay out with "rake routes"
 

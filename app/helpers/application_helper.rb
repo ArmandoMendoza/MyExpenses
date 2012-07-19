657ea39c2f2efad38a41411f855f8c7acac95988
Armando Mendoza <fcoarmandomendoza@gmail.com>
2012-07-18 21:01:35 -0430

    Added styles to ExpenseController. Added Helpers for notice and navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+	def alert_type(name, msg)
+		type = name == :notice ? "success" : "error"
+		class_name = "alert alert-#{type}"
+		content_tag(:div, class: class_name) do
+			link_to("x", "#", class: "close", data: { dismiss: "alert" }) +
+			content_tag(:b, msg)
+		end
+	end
+
+	def navbar(title)
+		content_tag(:div, class: "navbar") do
+			content_tag(:div, class: "navbar-inner") do
+				content_tag(:div, class: "container") do
+					link_to(title,"#", class: "brand") +
+					content_tag(:ul, class: "nav") do
+						yield
+					end
+				end
+			end
+		end
+	end
 end

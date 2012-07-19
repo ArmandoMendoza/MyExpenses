19143976b1e4383ac770de52597552fd5d043706
Armando Mendoza <fcoarmandomendoza@gmail.com>
2012-07-19 11:47:04 -0430

    Added helper method for link to login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+	def link_for_login
+		if user_signed_in?
+			link = link_to('Logout', destroy_user_session_path, :method => :delete)
+		else
+			link = link_to('Login', new_user_session_path)
+		end
+	end
+
 	def link_icon_to(name = "", icon = "", path = "#",  options_link = {})
 		link_to(path, options_link) do
 			content_tag(:i, "", class: icon) + " #{name}"

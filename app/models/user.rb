15418cdbfa0e6dce7fe1d4d4544ceb7bb920d97f
Armando Mendoza <fcoarmandomendoza@gmail.com>
2012-07-20 12:47:51 -0430

    Refactor in view of devise form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable, recoverable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
 
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
